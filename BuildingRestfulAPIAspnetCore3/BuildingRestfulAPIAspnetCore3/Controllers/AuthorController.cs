bf795c6438ca67a5d07ed3d104bcdc5617db4e05
unknown <tharana.maya@gmail.com>
2020-04-22 22:17:12 +0530

    created api project and create  GetAuthors request

---
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildingRestfulAPIAspnetCore3.Controllers
+{
+    [ApiController]
+    //[Route("api/authors")]
+    [Route("api/[controller]")]
+    public class AuthorController: ControllerBase
+    {   
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? 
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+
+
+    }
+}
