d0eb372e346d04cb61c6cb044a909409ed5d3a1e
unknown <tharana.maya@gmail.com>
2020-04-23 20:33:13 +0530

    returning single child resource

--- BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/CourseController.cs
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/CourseController.cs
@@ -32,6 +32,23 @@ namespace BuildingRestfulAPIAspnetCore3.Controllers
             var coursesForAuthorFromRepo = _courseLibraryRepository.GetCourses(authorId);
             return Ok(_mapper.Map<IEnumerable<CourseDto>>(coursesForAuthorFromRepo));
         }
+        [HttpGet("{courseId}")]
+        public ActionResult<CourseDto> GetCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
+
+            if (courseForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
+        }
 
 
     }
