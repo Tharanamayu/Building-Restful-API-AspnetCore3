b507246f3f6571b054e908a7a51732cd2edbf06d
unknown <tharana.maya@gmail.com>
2020-04-25 23:27:13 +0530

    customizing error message

--- BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Models/CourseForCreationDto.cs
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Models/CourseForCreationDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuildingRestfulAPIAspnetCore3.ValidationAttributes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,20 +7,22 @@ using System.Threading.Tasks;
 
 namespace BuildingRestfulAPIAspnetCore3.Models
 {
-    public class CourseForCreationDto : IValidatableObject
-    {   [Required]
-        [MaxLength(100)]
+    [CourseTitleMustBeDifferentFromDescriptionAttribute
+        (ErrorMessage ="Title  must be different from description")]
+    public class CourseForCreationDto //: IValidatableObject
+    {   [Required(ErrorMessage ="You should fill out a title")]
+        [MaxLength(100,ErrorMessage ="The title shouldn't have more than 100 characters.")]
         public string Title { get; set; }
-        [MaxLength(1500)]
+        [MaxLength(1500,ErrorMessage = "The Desciption shouldn't have more than 100 characters.")]
         public string Description { get; set; }
 
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (Title==Description)
-            {
-                yield return new ValidationResult(
-                    "The provided desciptionshould be different from the title", new[] { "CourseForCreationDto" });
-            }
-        }
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Title==Description)
+        //    {
+        //        yield return new ValidationResult(
+        //            "The provided desciptionshould be different from the title", new[] { "CourseForCreationDto" });
+        //    }
+        //}
     }
 }
