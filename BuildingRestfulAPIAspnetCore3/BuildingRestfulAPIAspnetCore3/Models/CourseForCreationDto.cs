60eb7f8afb946c0e7f956a6cd8b74704d3456d66
unknown <tharana.maya@gmail.com>
2020-04-25 22:08:04 +0530

    validating input with data annotation and class level input validation with Ivalidatable object

--- BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Models/CourseForCreationDto.cs
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Models/CourseForCreationDto.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BuildingRestfulAPIAspnetCore3.Models
 {
-    public class CourseForCreationDto
-    {
+    public class CourseForCreationDto : IValidatableObject
+    {   [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
-
+        [MaxLength(1500)]
         public string Description { get; set; }
 
-        public Guid AuthorId { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title==Description)
+            {
+                yield return new ValidationResult(
+                    "The provided desciptionshould be different from the title", new[] { "CourseForCreationDto" });
+            }
+        }
     }
 }
