ed256c8a65d84f96f11281a49e16e32a9a603e75
unknown <tharana.maya@gmail.com>
2020-04-25 18:41:42 +0530

    working with array keys and composite key

--- BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/AuthorCollectionsController.cs
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/AuthorCollectionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using BuildingRestfulAPIAspnetCore3.Models;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -22,6 +23,22 @@ namespace BuildingRestfulAPIAspnetCore3.Controllers
             _mapper = mapper ??
                 throw new ArgumentNullException(nameof(mapper));
         }
+        [HttpGet("{ids}",Name ="getAuthorCollection")]
+        public IActionResult GetAuthorCollection([FromRoute][ModelBinder(BinderType =typeof(ArrayModelBinder))]IEnumerable<Guid> ids)
+        {
+            if (ids ==null)
+            {
+                return BadRequest();
+            }
+            var authorEntities = _courseLibraryRepository.GetAuthors(ids);
+
+            if (ids.Count() != authorEntities.Count())
+            {
+                return NotFound();
+            }
+            var authorsToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            return Ok(authorsToReturn);
+        }
         [HttpPost]
         public ActionResult<IEnumerable<AuthorDto>> CreateAuthorCollection(IEnumerable<AuthorForCreationDto> authorCollection)
         {
@@ -32,7 +49,11 @@ namespace BuildingRestfulAPIAspnetCore3.Controllers
             }
 
             _courseLibraryRepository.Save();
-            return Ok();
+            var authorCollectionToReturn = _mapper.Map<IEnumerable<AuthorDto>>(authorEntities);
+            var idsAsString = string.Join(",", authorCollectionToReturn.Select(a => a.Id));
+            return CreatedAtRoute("GetAuthorCollection",
+             new { ids = idsAsString },
+             authorCollectionToReturn);
         }
     }
 }
