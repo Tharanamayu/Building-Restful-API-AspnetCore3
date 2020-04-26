75ec1ddf33dd5b379fa27c83009c6d60de40960d
unknown <tharana.maya@gmail.com>
2020-04-26 17:27:13 +0530

    Deleting a resource with child resources

--- BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/CourseController.cs
+++ BuildingRestfulAPIAspnetCore3/BuildingRestfulAPIAspnetCore3/Controllers/CourseController.cs
@@ -141,6 +141,26 @@ namespace BuildingRestfulAPIAspnetCore3.Controllers
             return NoContent();
 
         }
+        [HttpDelete("{courseId}")]
+        public ActionResult DeleteCourseForAuthor(Guid authorId, Guid courseId)
+        {
+            if (!_courseLibraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var courseForAuthorFromRepo = _courseLibraryRepository.GetCourse(authorId, courseId);
+
+            if (courseForAuthorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            _courseLibraryRepository.DeleteCourse(courseForAuthorFromRepo);
+            _courseLibraryRepository.Save();
+
+            return NoContent();
+        }
         public override ActionResult ValidationProblem(
                   [ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
         {
